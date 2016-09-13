386042dafbe7b98393fccfb9852cd29fb59397e0
harrisondurant <hdurant2@elon.edu>
2016-09-13 15:07:10 -0400

    Added Flexbox design, new sounds

--- 1234.js
+++ 1234.js
@@ -3,8 +3,8 @@ var bufferLoader;
 var myBufferList;
 var soundMap = {};
 var loops = {};
-var names = ["explore_kick_1","explore_kick_2","explore_kick_3","Air_funk","Caddy_ki","Doggiek","fatkick","Glitch Kick","kick_FSMH","Ohiokic","explore_clap","explore_snap1","explore_snap2","explore_snare1","explore_snare2","explore_snare3","BALTIMOR","Newerks","s2","s3","sn1","snr_FSMH","tightsnare","clap","explore_chop","chop1","chop2","chop4","stab62","Sound192","chimes","FX9","explore_fx","explore_perc","zap1","hih1","hih2","hh1","hh2","crash","crash1","crash2","airhorn","ironside","another-one","hah","haan","milli","montana","shout","scream1","[VOX] wooh","[VOX] Perfect"];
-var soundBank = ["sounds/explore_kick_1.mp3","sounds/explore_kick_2.mp3","sounds/explore_kick_3.mp3","sounds/Air_funk.mp3","sounds/Caddy_ki.mp3","sounds/Doggiek.mp3","sounds/fatkick.mp3","sounds/Glitch_Hop_Kick05.mp3","sounds/(Kick) FSMH1.mp3","sounds/Ohiokic.mp3","sounds/explore_clap.mp3","sounds/explore_snap1.mp3","sounds/explore_snap2.mp3","sounds/explore_snare1.mp3","sounds/explore_snare2.mp3","sounds/explore_snare3.mp3","sounds/BALTIMOR.mp3","sounds/Newerks.mp3","sounds/s2.mp3","sounds/s3.mp3","sounds/sn1.mp3","sounds/[Snr] BEAUTIFUL MORNIN.mp3","sounds/tightsnare.mp3","sounds/clap.mp3","sounds/explore_chop.mp3","sounds/chop1.mp3","sounds/chop2.mp3","sounds/chop4.mp3","sounds/Misc_Stabs_62.mp3","sounds/Sound192.mp3","sounds/chimes.mp3","sounds/FX9.mp3","sounds/explore_fx.mp3","sounds/explore_perc.mp3","sounds/zap1.mp3","sounds/hih1.mp3","sounds/hih2.mp3","sounds/hh1.mp3","sounds/hh2.mp3","sounds/crash.mp3","sounds/crash1.mp3","sounds/crash2.mp3","sounds/airhorn.mp3","sounds/ironside.mp3","sounds/another-one.mp3","sounds/hah.mp3","sounds/haan.mp3","sounds/milli.mp3","sounds/montana.mp3","sounds/shout.mp3","sounds/scream1.mp3","sounds/[VOX] BBB4U Quickie.mp3","sounds/[VOX] Perfect.mp3","sounds/loop_calabria.mp3","sounds/loop_hats.mp3","sounds/loop_hiphop.mp3","sounds/loop_sango.mp3"];
+var names = ["explore_kick_1","explore_kick_2","explore_kick_3","Air_funk","Caddy_ki","Doggiek","fatkick","Glitch Kick","kick_FSMH","Ohiokic","explore_clap","explore_snap1","explore_snap2","explore_snare1","explore_snare2","explore_snare3","BALTIMOR","Newerks","s2","s3","sn1","snr_FSMH","tightsnare","clap","explore_chop","chop1","chop2","chop4","stab62","Sound192","chimes","FX9","explore_fx","wheeee_fx","explore_perc","zap1","hih1","hih2","hh1","hh2","crash","crash1","airhorn","ironside","another-one","100","hah","hey!","haaahn","milli","montana","shout","scream1","[VOX] wooh","[VOX] Perfect"];
+var soundBank = ["sounds/explore_kick_1.mp3","sounds/explore_kick_2.mp3","sounds/explore_kick_3.mp3","sounds/Air_funk.mp3","sounds/Caddy_ki.mp3","sounds/Doggiek.mp3","sounds/fatkick.mp3","sounds/Glitch_Hop_Kick05.mp3","sounds/(Kick) FSMH1.mp3","sounds/Ohiokic.mp3","sounds/explore_clap.mp3","sounds/explore_snap1.mp3","sounds/explore_snap2.mp3","sounds/explore_snare1.mp3","sounds/explore_snare2.mp3","sounds/explore_snare3.mp3","sounds/BALTIMOR.mp3","sounds/Newerks.mp3","sounds/s2.mp3","sounds/s3.mp3","sounds/sn1.mp3","sounds/[Snr] BEAUTIFUL MORNIN.mp3","sounds/tightsnare.mp3","sounds/clap.mp3","sounds/explore_chop.mp3","sounds/chop1.mp3","sounds/chop2.mp3","sounds/chop4.mp3","sounds/Misc_Stabs_62.mp3","sounds/Sound192.mp3","sounds/chimes.mp3","sounds/FX9.mp3","sounds/explore_fx.mp3","sounds/whee_fx.mp3","sounds/explore_perc.mp3","sounds/zap1.mp3","sounds/hih1.mp3","sounds/hih2.mp3","sounds/hh1.mp3","sounds/hh2.mp3","sounds/crash.mp3","sounds/crash1.mp3","sounds/airhorn.mp3","sounds/ironside.mp3","sounds/another-one.mp3","sounds/100.mp3","sounds/hah.mp3","sounds/hey.mp3","sounds/haan.mp3","sounds/milli.mp3","sounds/montana.mp3","sounds/shout.mp3","sounds/scream1.mp3","sounds/[VOX] BBB4U Quickie.mp3","sounds/[VOX] Perfect.mp3","sounds/loop_calabria.mp3","sounds/loop_hats.mp3","sounds/loop_crowd.mp3","sounds/loop_sango.mp3"];
 var buttonNames = ['one','two','three','four','five'];
 var buttonList = [1,2,3,4,5];
 var keyList = [49,50,51,52,53];
@@ -23,9 +23,6 @@ window.requestAnimFrame = (function(){
 window.onload = init;
 
 function init() {
-
-  //setupTextBox();
-
   context = new (window.AudioContext || window.webkitAudioContext)();
   bufferLoader = new BufferLoader(context,soundBank,finishedLoading);
   bufferLoader.load();
@@ -50,9 +47,8 @@ function finishedLoading(bufferList) {
 }
 
 function changeSound(number, soundName) {
-//  alert("button " + number + "\'s sound changed to " + soundName);
   if(soundMap[number].playing) { //if sound is already playing 
-    stopSound(number);           //stop it before creating and playing the new buffer
+    soundMap[number].stop(); //stop it before creating and playing the new buffer
   }
 
   var idx = names.indexOf(soundName);
@@ -61,8 +57,8 @@ function changeSound(number, soundName) {
   var button = document.getElementById(""+buttonList[number]);
   button.style.backgroundColor = "rgba(0,230,255,0.85)";
 
-  var label = document.getElementById("label"+buttonList[number]);
-  label.innerHTML = soundName;
+  var labels = document.querySelectorAll('.padLabel > span');
+  labels[number].innerHTML = soundName;
 }
 
 function onKeyUp(key) {
@@ -72,7 +68,6 @@ function onKeyUp(key) {
     var button = document.getElementById(""+buttonList[number]);
     button.style.backgroundColor = "rgba(0,230,255,0.85)";
     button.style.color = "black";
-    //button.style.backgroundColor = "rgba("+bg[0]+","+bg[1]+","+bg[2]+",0.6)";
   }
 }
 
@@ -81,7 +76,7 @@ function onKeyDown(key) {
   if(soundMap[number].buffer != undefined) {
     if(soundMap[number].can_trigger) {
       var button = document.getElementById(""+buttonList[number]);
-      play(number,0);
+      play(number);
       button.style.backgroundColor = "rgba(0,170,200,0.7)";
       button.style.color = "rgba(0,230,255,0.6)"; //visual 'press' effect
     }
@@ -90,43 +85,23 @@ function onKeyDown(key) {
 
 function toggleLoop(number) {
   var curr_loop = document.getElementById('loop'+(number+1));
-  if(loops[number].playing) { //if sound is already playing 
-    loops[number].playing = false;
-    loops[number].sound.disconnect(context.destination); //stop it
+  if(loops[number].playing) { //if loop is playing 
+    loops[number].stop(); //stop it
     curr_loop.style.backgroundColor = "rgba(100,100,100,0.5)";
   }
   else {
-    loops[number].playing = true;
-    loops[number].sound = context.createBufferSource();
-    loops[number].sound.loop = true;
-    loops[number].sound.buffer = loops[number].buffer;
-    loops[number].sound.connect(context.destination);
-    loops[number].sound.start(0);
+    loops[number].play(true);
     curr_loop.style.backgroundColor = "rgba(100,255,100,0.7)";
   }
 }
 
 function play(number) {
   if(soundMap[number].playing) { //if sound is already playing 
-    stopSound(number);           //stop it before creating and playing the new buffer
+    soundMap[number].stop(); //stop it before creating and playing the new buffer
   }
   soundMap[number].playing = true;
   soundMap[number].can_trigger = false;
-  playSound(number,0);
-}
-
-function playSound(number,time) {
-  soundMap[number].playing = true;
-  soundMap[number].sound = context.createBufferSource();
-  soundMap[number].sound.buffer = soundMap[number].buffer;
-  soundMap[number].sound.connect(context.destination);
-  soundMap[number].sound.start(time);
-  //soundMap[number].sound.playbackRate.value = soundMap[number].rate;
-}
-
-function stopSound(number) {
-  soundMap[number].playing = false;
-  soundMap[number].sound.disconnect(context.destination);
+  soundMap[number].play();
 }
 
 function isValidKey(key) {
@@ -142,6 +117,20 @@ function Sound(sound,buffer,rate,playing,can_trigger) {
   this.can_trigger = can_trigger;
 }
 
+Sound.prototype.play = function(loop) {
+  this.playing = true;
+  this.sound = context.createBufferSource();
+  this.sound.buffer = this.buffer;
+  this.sound.connect(context.destination);
+  this.sound.loop = loop;
+  this.sound.start(0);
+}
+
+Sound.prototype.stop = function() {
+  this.playing = false;
+  this.sound.disconnect(context.destination);
+}
+
 function setupLoops() {
   for(var i = 0; i < 4; i++) {
     loops[i] = new Sound(null,myBufferList[myBufferList.length-1-i],1.0,false,true);
@@ -150,13 +139,21 @@ function setupLoops() {
   var display_loops = document.getElementById('loops_wrapper');
   var loading = document.getElementById('loading_loops');
   loops_div.removeChild(loading);
-  display_loops.style.display = "block";
+  display_loops.style.display = "flex";
 }
 
 function setupButtons() {
   for(var i = 0; i < 5; i++) {
     soundMap[i] = new Sound(null,null,1.0,false,true);
   }
+}
+
+function showSounds() {
+  var wrapper = document.getElementById('sounds_wrapper');
+  var sounds = document.getElementById('sounds');
+  var loading = document.getElementById('loading_sounds');
+  wrapper.removeChild(loading);
+  sounds.style.display = "flex";
 }
 
 function setupLists() {
@@ -172,25 +169,6 @@ function setupLists() {
     list.appendChild(soundLabel);
   }
 }
-
-function showSounds() {
-  var wrapper = document.getElementById('sounds_wrapper');
-  var sounds = document.getElementById('sounds');
-  var loading = document.getElementById('loading');
-  wrapper.removeChild(loading);
-  sounds.style.display = "block";
-}
-
-// function setupTextBox() {
-//   var textBox = document.getElementById('text');
-//   textBox.style.display = "block";
-//   var span = document.getElementsByClassName("close")[0];
-
-//   // When the user clicks on <span> (x), close the modal
-//   span.onclick = function() {
-//     textBox.style.display = "none";
-//   }
-// }
 
 function setupDrag() {
   var pads = document.querySelectorAll('#mpc button');
@@ -216,18 +194,12 @@ function handleDragOver(e) {
 
 // to get IE to work
 function handleDragEnter() {
-  this.className = 'over';
   return false;
 }
 
 function handleDragLeave() {
-  //this.className = 'btn';
-  if(soundMap[this.id-1].buffer != undefined) {
-    this.style.backgroundColor = "rgba(0,230,255,0.85)";
-  }
-  else {
-    this.style.backgroundColor = "rgba(100,100,100,0.9)";
-  }
+  var undef = soundMap[this.id-1].buffer != undefined;
+  this.style.backgroundColor = undef ? "rgba(0,230,255,0.85)" : "rgba(100,100,100,0.9)";
 }
 
 function handleDrop(e) {
@@ -238,7 +210,6 @@ function handleDrop(e) {
   var el = document.getElementById(e.dataTransfer.getData('Text'));
   changeSound(this.id-1, el.id);
 }
-
 
 /*
 
@@ -252,8 +223,3 @@ better button presses
 iOS capable
 
 */
-
-
-
-
-
